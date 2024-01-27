97ae50dc2e22737c46645b7f1a1487d5f384d84d
Randy Gordey <Randy.Gordey@ade.arkansas.gov>
2024-01-27 10:46:21 -0600

    Added DbContext

---
+++ AutoMapperDemo/ApplicationDbContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace AutoMapperDemoData
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<ProductComplex> ProductsComplex { get; set; } = default!;
+        public DbSet<ProductOwned> ProductSOwned { get; set; } = default!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.;Database=AutoMapperDb;Trusted_Connection=True;TrustServerCertificate=True;");
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ProductComplex>(x => 
+                x.ComplexProperty(y => y.Dimension, y => { y.IsRequired();}));
+
+            modelBuilder.Entity<ProductOwned>(x => x.OwnsOne(y => y.Dimension));
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- AutoMapperDemoUI/Program.cs
+++ AutoMapperDemoUI/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using AutoMapperDemoData;
+using AutoMapperDemoUI;
+
 Console.WriteLine("Hello, World!");
+
+var config = new MapperConfiguration(cfg => {
+    cfg.CreateMap<ProductOwned, ProductOwnedDto>().ReverseMap();
+    cfg.CreateMap<ProductComplex, ProductComplexDto>().ReverseMap();
+    cfg.CreateMap<Dimension, DimensionDto>().ReverseMap();
+});
+
+var context = new ApplicationDbContext();
+
+var first = context.ProductSOwned.ProjectTo<ProductOwnedDto>(config).ToList();
+var second = context.ProductsComplex.ProjectTo<ProductComplexDto>(config).ToList();
